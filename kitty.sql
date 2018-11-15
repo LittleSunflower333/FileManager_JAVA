66e4d1616e4285a6ecb35a5999f15abacdf8e161
javawebservlet <javawebservlet@126.com>
2018-11-15 13:36:02 +0800

    demo

---
+++ kitty.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v12.3.1 (64 bit)
+MySQL - 5.6.36 : Database - kitty
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`kitty` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `kitty`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `user_name` varchar(50) NOT NULL COMMENT '用户名',
+  `password` varchar(100) DEFAULT NULL COMMENT '密码',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1062916940776337410 DEFAULT CHARSET=utf8 COMMENT='用户';
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`user_name`,`password`) values 
+(1062916940776337409,'admin','654321');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
