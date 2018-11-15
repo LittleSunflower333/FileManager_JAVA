07866505cb6a936d00d6287cb886a906113e566b
javawebservlet <javawebservlet@126.com>
2018-11-15 13:35:15 +0800

    demo

---
+++ src/main/java/com/dalaoyang/controller/DictController.java
@@ -0,0 +1,20 @@
+package com.dalaoyang.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: springboot_mybitsplus_swagger
+ * @description: 字典管理
+ * @author: Mr.Wang
+ * @create: 2018-11-15 13:28
+ **/
+@Api(description = "字典接口")
+@RestController
+@RequestMapping("/dict")
+public class DictController {
+
+
+
+}
